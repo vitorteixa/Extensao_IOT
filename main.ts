5afc3255eb2542c7fbb906bc04d450445d49d943
vitorteixa <33202529+vitorteixa@users.noreply.github.com>
2022-08-20 10:04:48 -0300

    Teste de sensores de luminosidade e buser

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
 	
+})
+basic.forever(function () {
+    basic.showNumber(0)
+    if (input.lightLevel() < 50) {
+        music.playMelody("C5 B A G F E D C ", 120)
+        basic.showString("Está escuro")
+    } else {
+        basic.showString("Está Claro")
+    }
 })
